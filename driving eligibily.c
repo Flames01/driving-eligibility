96e018fb081843fd60fc66a7f1dfa8f76f737f6a
jyoti kushwaha <98680848+Flames01@users.noreply.github.com>
2022-02-01 10:10:52 +0530

    Add files via upload

---
+++ driving eligibily.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{  printf("%%%%%%%________check your age for driving_____%%%%%%\n");
+  int age;
+  printf(" enter your age\n");
+  scanf("%d",&age);
+  
+  if(age>=18){
+  printf(" you are eligible for driving. \n");
+  printf("*****_______you can drive_______********* .\n \n");
+      printf("^^^^^^^_______don't drink and drive. _____^^^^^^\n\n");}
+  else {
+  printf("you're not eligible for driving. \n");
+      printf("######____you can't drive.________#######\n\n");
+      printf("^^^^^^^________wait for some time.______^^^^^^^\n\n");
+      
+  }
+    printf("&&&&&&&_______thnx for coming here. ______&&&&&&&&\n\n");
+ printf("**********________have a great journey_______***********\n\n");
+  return 0;
+ 
+}
